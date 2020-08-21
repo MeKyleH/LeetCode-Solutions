e26a27e965a6a9efe03484ebdda35497a2ddb1fa
Kyle Holt <kyleholt@Kyles-MacBook-Pro.local>
2020-08-21 12:15:46 -0600

    didn't save file code

--- AlgoExpert Very Hard/Merge Sort.js
+++ AlgoExpert Very Hard/Merge Sort.js
@@ -1,42 +1,65 @@
 /**
-Move Element To End
+Merge Sort
 
-You're given an array of integers and an integer. Write a function that moves
-all instances of that integer in the array to the end of the array and returns
-the array.
+Write a function that takes in an array of integers and returns a sorted
+version of that array. Use the Merge Sort algorithm to sort the array.
 
-The function should perform this in place (i.e., it should mutate the input
-array) and doesn't need to maintain the order of the other integers.
-
-Example 1:
+Example:
 Input:
-array = [2, 1, 2, 2, 2, 3, 4, 2];
-toMove = 2;
-
+array = [8, 5, 2, 9, 5, 6, 3]
 Output:
-[1, 3, 4, 2, 2, 2, 2, 2]
+[2, 3, 5, 5, 6, 8, 9]
 **/
 
-function moveElementToEnd(array, toMove) {
-	let matchesFound = 0;
-	// loop through the array
-	for (let i = 0; i < array.length; i++) {
-		// end if we've gone far enough that we've checked all matches
-		if (i === array.length - 1 - matchesFound) {
-			break;
-		}
-		// check if this number needs to move
-		if (array[i] === toMove) {
-			// remove this number from the array
-			// add this number to the end of the array
-			array.splice(i, 1);
-			array.push(toMove);
-			matchesFound++;
-			i--;
+// O(nlog(n)) Time O(nlog(n)) Space
+function mergeSort(array) {
+	// base case
+	if (array.length <= 1) {
+		return array;
+	}
+	// divide the array into two halfs
+	const halfIndex = Math.floor(array.length / 2);
+
+	const firstHalf = array.slice(0, halfIndex);
+	const secondHalf = array.slice(halfIndex);
+
+	// join the final splitted arrays
+	return joinSortedArrays(mergeSort(firstHalf), mergeSort(secondHalf));
+}
+
+function joinSortedArrays(firstHalf, secondHalf) {
+	const sortedArray = [];
+	let pointer1 = 0;
+	let pointer2 = 0;
+
+	// repeat until we get through one of the arrays
+	while (pointer1 < firstHalf.length && pointer2 < secondHalf.length) {
+		const value1 = firstHalf[pointer1];
+		const value2 = secondHalf[pointer2];
+
+		// add the lesser value to the final sorted array
+		if (value1 <= value2) {
+			sortedArray.push(value1);
+			pointer1++;
+		} else {
+			sortedArray.push(value2);
+			pointer2++
 		}
 	}
-	return array;
+
+	// repeat for the longer array of the two
+	while (pointer1 < firstHalf.length) {
+		sortedArray.push(firstHalf[pointer1]);
+		pointer1++;
+	}
+
+	while (pointer2 < secondHalf.length) {
+		sortedArray.push(secondHalf[pointer2]);
+		pointer2++
+	}
+
+	return sortedArray;
 }
 
 // Do not edit the line below.
-exports.moveElementToEnd = moveElementToEnd;
+exports.mergeSort = mergeSort;
